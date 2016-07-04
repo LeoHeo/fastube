50431eee6fd901ce68927a9eaf0ed9521c9f21cd
LeoHeo <hjh5488@gmail.com>
2016-07-04 22:56:31 +0800

    update database setting: using dj_database_url

--- fastube/fastube/settings/partials/database.py
+++ fastube/fastube/settings/partials/database.py
@@ -1,13 +1,11 @@
+# dj_datbase_url
+# https://github.com/kennethreitz/dj-database-url
+
+import dj_database_url
 import os
 
-from .base import BASE_DIR
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get("DATABASE_URL")
+)
