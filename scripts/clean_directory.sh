0b714a18932e14116ca4aad5475f65aafa333454
LeoHeo <hjh5488@gmail.com>
2016-07-05 02:34:50 +0800

    add pyenv direct install

--- scripts/clean_directory.sh
+++ scripts/clean_directory.sh
@@ -1,4 +1,10 @@
-source /home/ubuntu/.bash_profile
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv update
+echo 'export PATH="~/.pyenv/bin:$PATH"' > .bash_profile
+echo 'eval "$(pyenv init -)"' > .bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"'> .bash_profile
+
+pyenv install 3.5.1
 pyenv virtualenv 3.5.1 fastube
 rm -rf /home/ubuntu/fastube
 mkdir /home/ubuntu/fastube
